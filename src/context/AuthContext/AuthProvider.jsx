3a1c68fc1c1da5db38c0713d4180063fe2692c1a
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-12-17 18:28:43 -0500

    first commit

---
+++ src/context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import AuthContext from './AuthContext';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import auth from '../../firebase/firebase.init';
+import axios from 'axios';
+
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const singInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const singInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const signOutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('state captured, current user', currentUser);
+
+            if (currentUser?.email) {
+                const user = { email: currentUser.email }
+
+                axios.post('http://localhost:5000/jwt', user, {
+                    withCredentials: true
+                })
+                    .then(res => {
+                        console.log('login token', res.data)
+                        setLoading(false);
+                    })
+                //make sure to see "login token, success:true" and the token in application tab
+            }
+
+            // when the user logs out, the currentUser becomes null
+            else {
+                axios.post('http://localhost:5000/logout', {}, {
+                    withCredentials: true,
+                })
+                    .then(res => {
+                        console.log('logout', res.data)
+                        setLoading(false);
+                    })
+                //make sure to see "logout, success:true" and no token in application tab
+            }
+
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        singInUser,
+        singInWithGoogle,
+        signOutUser
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
